71671217910e4cdd7d43ed4b2f31a597f938a715
Mudzanani Tshimangadzo <mudzanani27@gmail.com>
2023-01-07 20:40:43 +0200

    changed heading to active learning project

--- ALP_UI/src/app/components/account/login/login.component.ts
+++ ALP_UI/src/app/components/account/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { Router, ActivatedRoute } from "@angular/router";
 
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder,FormGroup, Validators } from '@angular/forms';
 import { first } from "rxjs/operators";
 
 import { AccountService,AlertService } from "src/app/services/account";
